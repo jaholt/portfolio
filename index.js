14bc929fc857a458fed0be8a78dc9c69d3223fca
Joe Holt <holtj@JosephXPSTower.private.halfways.net>
2020-10-28 16:10:02 +0000

    played around with an interesting loading page

---
+++ index.js
@@ -0,0 +1,11 @@
+setTimeout(function loadPageAnimation () {
+    document.getElementById('loaderContainer').hidden = true
+    document.getElementById('titleContainer').hidden = false
+}, 5000)
+
+
+function enterClick () {
+    document.getElementById('enterText').onclick(() => {
+        document.getElementById('')
+    })
+}
